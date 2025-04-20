5c8c591ddba4b92d7b091a96aaaeabc700b2c4ae
Yashwant-web <yashwant0998@gmail.com>
2025-04-20 01:24:24 +0100

    Refactored LibrarianSuccess form to improve code clarity and reduce repetition in button actions

--- src/mainlibrary/LibrarianSuccess.java
+++ src/mainlibrary/LibrarianSuccess.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainlibrary;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  * public static JFrame ThisLogined;
@@ -18,12 +14,11 @@ import javax.swing.JFrame;
 public class LibrarianSuccess extends javax.swing.JFrame {
 
     public static JFrame ThisLogined;
+    public static String Name, LibrarianID, Email;
 
     public JFrame GetLibrarianLogin() {
-        return ThisLogined;//To change body of generated methods, choose Tools | Templates.
+        return ThisLogined; // To change body of generated methods, choose Tools | Templates.
     }
-
-    public static String Name, LibrarianID, Email;
 
     /**
      * Creates new form LibrarianSuccess
@@ -67,42 +62,44 @@ public class LibrarianSuccess extends javax.swing.JFrame {
         jButton2.setText("Issue Book");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.IssueBookForm");
             }
         });
 
         jButton3.setText("Return Book");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.ReturnBookForm");
             }
         });
 
         jButton4.setText("Delete Book");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.DeleteBook");
             }
         });
 
         jButton6.setText("View Students");
         jButton6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton6ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.AllStudent");
             }
         });
 
-        jButton7.setText("LogOut");
+                jButton7.setText("LogOut");
         jButton7.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton7ActionPerformed(evt);
+                // Close the current frame and go to MainLibrary
+                LibrarianSuccess.this.dispose();  // Dispose the current frame (LibrarianSuccess)
+                MainLibrary.main(new String[]{});
             }
         });
 
         jButton8.setText("Add Student");
         jButton8.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton8ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.UserForm");
             }
         });
 
@@ -118,47 +115,27 @@ public class LibrarianSuccess extends javax.swing.JFrame {
         jTextField1.setBackground(new java.awt.Color(254, 254, 254));
         jTextField1.setDisabledTextColor(new java.awt.Color(4, 3, 2));
         jTextField1.setEnabled(false);
-        jTextField1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                jTextField1MousePressed(evt);
-            }
-        });
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
 
         jTextField2.setEditable(false);
         jTextField2.setBackground(new java.awt.Color(254, 254, 254));
         jTextField2.setDisabledTextColor(new java.awt.Color(6, 4, 2));
         jTextField2.setEnabled(false);
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
-            }
-        });
 
         jTextField3.setEditable(false);
         jTextField3.setDisabledTextColor(new java.awt.Color(1, 1, 1));
         jTextField3.setEnabled(false);
-        jTextField3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
-            }
-        });
 
         jButton5.setText("Add Book");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.BookForm");
             }
         });
 
         jButton1.setText("View Book");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.ViewBook");
             }
         });
 
@@ -217,7 +194,7 @@ public class LibrarianSuccess extends javax.swing.JFrame {
         jButton9.setText("View Issued Books");
         jButton9.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton9ActionPerformed(evt);
+                handleButtonAction(evt, "mainlibrary.NewView");
             }
         });
 
@@ -271,81 +248,20 @@ public class LibrarianSuccess extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-
-        ViewBook.main(new String[]{});
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-        IssueBookForm.main(new String[]{});
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-        ReturnBookForm.main(new String[]{});
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-        DeleteBook.main(new String[]{});
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-        BookForm.main(new String[]{});
-    }//GEN-LAST:event_jButton5ActionPerformed
-
-    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        // TODO add your handling code here:
-        this.dispose();
-        MainLibrary.main(new String[]{});
-    }//GEN-LAST:event_jButton7ActionPerformed
-
-    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        // TODO add your handling code hereer
-        this.setVisible(false);
-        UserForm.main(new String[]{});
-    }//GEN-LAST:event_jButton8ActionPerformed
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
-
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField3ActionPerformed
-
-    private void jTextField1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTextField1MousePressed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1MousePressed
-
-    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-        // TODO add your handling code here:
-        NewView.main(new String[]{});
-    }//GEN-LAST:event_jButton9ActionPerformed
-
-    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
-        AllStudent.main(new String[]{});
-    }//GEN-LAST:event_jButton6ActionPerformed
+    private void handleButtonAction(java.awt.event.ActionEvent evt, String formClassName) {
+        try {
+            Class<?> formClass = Class.forName(formClassName);
+            formClass.getMethod("main", String[].class).invoke(null, (Object) new String[]{});
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Error opening form: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
-    static void main(String args[]) {//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+    static void main(String args[]) {
+        /* Set the Nimbus look and feel */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -362,18 +278,15 @@ public class LibrarianSuccess extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(LibrarianSuccess.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Set the Nimbus look and feel */
- /* Create and display the form */
+        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-
                 ThisLogined = new LibrarianSuccess();
                 ThisLogined.setVisible(true);
-
             }
         });
+
         String User = args[0];
         String Pass = args[1];
         try {
@@ -391,18 +304,16 @@ public class LibrarianSuccess extends javax.swing.JFrame {
             Email = rs.getString("Email");
             System.out.println(Name + " " + LibrarianID + " " + Email);
             Con.close();
-
         } catch (Exception f) {
             System.out.println(f);
         }
 
     }
 
-    /**
-     *
-     */
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -417,8 +328,5 @@ public class LibrarianSuccess extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private static javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
